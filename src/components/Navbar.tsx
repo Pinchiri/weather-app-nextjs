6b0c4fea9514bde079bc9b53536e79cf18ea219d
Rolando Sorrentino <rolando4693@gmail.com>
2024-08-27 11:47:35 -0400

    chore:🚀 Project created

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+  
+}
+
+const Navbar = ({}: Props) => {
+  return (
+    <nav className='shadow-sm sticky top-0 left-0 z-50 bg-white'>Navbar</nav>
+  )
+}
+
+export default Navbar
