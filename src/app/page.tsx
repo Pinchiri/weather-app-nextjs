dedc4ca48d0b9e791c210b8d4f224b6d848e4cd9
pinchiri <rolando.sorrentino@correo.unimet.edu.ve>
2024-08-28 14:36:15 -0400

    feat: :sparkles: SearchBox, cn function

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Navbar from "@/components/Navbar";
+import { Navbar } from "@/components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-4 min-h-screen">
       <Navbar />
-      
     </div>
   );
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { MdSunny } from "react-icons/md";
 import { MdMyLocation } from "react-icons/md";
+import { MdLocationOn } from "react-icons/md";
+import { SearchBox } from "../SearchBox/SearchBox";
 
 type Props = {};
 
-const Navbar = ({}: Props) => {
+export const Navbar = ({}: Props) => {
   return (
     <nav className="shadow-sm border-gray-200 border sticky top-0 left-0 z-50 bg-white">
       <div className="h-20 w-full flex justify-between items-center max-w-7xl px-3 mx-auto">
@@ -13,11 +15,14 @@ const Navbar = ({}: Props) => {
           <MdSunny className="text-3xl mt-1 text-yellow-200" />
         </span>
         <section className="flex gap-2 items-center">
-          <MdMyLocation className="" />
+          <MdMyLocation className="text-2xl text-blue-500 hover:text-blue-600 hover:scale-105 transition-all cursor-pointer" />
+          <MdLocationOn className="text-2xl text-red-500 hover:text-red-600 hover:scale-105 transition-all cursor-pointer" />
+          <p className="text-gray-900/80 text-sm mr-2">Venezuela</p>
+          <div>
+            <SearchBox onChange={() => {}} onSubmit={() => {}} value="" />
+          </div>
         </section>
       </div>
     </nav>
   );
 };
-
-export default Navbar;
